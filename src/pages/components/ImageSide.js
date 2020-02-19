3dc65c6e50c656dd6bf4b41d444d6f9e0f5cd755
tdnicola <tdnicola@gmail.com>
2020-02-18 22:08:23 -0500

    adding css for image side.

--- src/pages/components/ImageSide.js
+++ src/pages/components/ImageSide.js
@@ -3,14 +3,16 @@ import styled from 'styled-components';
 import Background from '../../../data/images/VigilImage.png'
 
 const AboutMe = styled.div`
-  width: 45%;
-  float: left
+    width: 50%;
+    float: left;
+    height: 100vh; 
+    background-image: linear-gradient(to bottom, rgba(245, 246, 252, 0.52), rgba(117, 19, 93, 0.73)),
+    url(${Background});
+    background-size: cover;
   `
 
 const ImageSide = () => (
-    <AboutMe className='imageSide'>
-            <img src={ Background } />
-    </AboutMe>
+    <AboutMe />
 )
 
 export default ImageSide
--- src/pages/components/Resume.js
+++ src/pages/components/Resume.js
@@ -2,43 +2,57 @@ import React from "react";
 import styled from 'styled-components';
 
 const Container = styled.div`
-  width: 45%;
-  float: right;
-  display: inline;
+  width: 85%;
+`
+
+const RightSide = styled.div`
+    width: 50%;
+    float: right;
+    display: inline;
+    background-color: lavenderblush;
+    `
+const Title = styled.span`
+    margin-right: 10px;   
 `
 
 const ResumeSide = ({education, mainProject, projects, coursework, work}) => (
-    <Container className='resumeSide'>
-        <div className='education'>
-            Education: {education}
-        </div>
-        <div className='mainProject'>
-            main project: {mainProject.title}
-            {mainProject.description.map((list) => {
-                return <li> {list} </li>
-            })}
-        </div>
-        <div className='projects'>
-            additional projects: 
-            {projects.map((project) => {
-                return <div className='additionalProjects'>Title: {project.title}
-                    <div>
-                        description: {project.description}
+    <RightSide>
+        <Container>
+            <div>
+                Education: {education}
+            </div>
+            <div className='mainProject'>
+                <Title>main project:</Title> {mainProject.title}
+                {mainProject.description.map((list) => {
+                    return <li> {list} </li>
+                })}
+            </div>
+            
+            <div className='projects'>
+                additional projects: 
+                {projects.map((project) => {
+                    return <div className='additionalProjects'>Title: {project.title}
+                        <div>
+                            description: {project.description}
+                        </div>
                     </div>
-                 </div>
-            })}
-        </div>
-        <div className='coursework'>
-            coursework: {coursework.map((courses) => {
-                return courses
-            })}
-        </div>
-        <div className='work'>
-            work experience: {work.map((jobs) => {
-                return jobs.company
-            })}
-        </div>
-    </Container>
+                })}
+            </div>
+
+            <div className='coursework'>
+                coursework: {coursework.map((courses) => {
+                    return courses
+                })}
+            </div>
+
+            <div className='work'>
+                work experience: {work.map((jobs) => {
+                    return jobs.company
+                })}
+            </div>
+
+        </Container>
+    </RightSide>
 )
 
 export default ResumeSide;
